e3086d2ff83f720d3da12c4d0a799df04f241ea5
Russell Gordon <rgordon@lcs.on.ca>
2025-02-26 20:54:37 -0500

    Initial Commit

---
+++ SpeedingFineCalculatorMVVM/SpeedingFineCalculatorMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedingFineCalculatorMVVMApp.swift
+//  SpeedingFineCalculatorMVVM
+//
+//  Created by Russell Gordon on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct SpeedingFineCalculatorMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
